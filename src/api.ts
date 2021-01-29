e364c62e4c6088e048d17fb2413107d4a7227f59
Lagoons Browning <lagoons@protonmail.com>
2021-01-28 20:41:58 -0600

    Changed favicon info, added movie database support for easier creation

--- src/api.ts
+++ src/api.ts
@@ -19,7 +19,7 @@ export async function getUsers(): Promise<IUser> {
     return await sendApiRequest("GET", "users", false);
 }
 
-export async function register(email: Partial<IUser>, username: Partial<IUser>, password: Partial<IUser>): Promise<IUser> {
+export async function register(email: string, username: string, password: string): Promise<IUser> {
     return await sendApiRequest("POST", "users/register", false, {username: username, email: email, password: password, profilePicture: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"})
 }
 
@@ -51,9 +51,45 @@ export async function deleteMovie(id: string): Promise<IMovie> {
     return await sendApiRequest("DELETE", `movies/delete/${id}`, false);
 }
 
+export async function searchMovies(query: string) {
+    try {
+        let encoded: string = encodeURIComponent(query)
+        const response:IMovieSearch = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=071fef7c3850e650d6753b753e9b880e&language=en-US&query=${encoded}&page=1&include_adult=false`)
+        return response.data.results
+    }catch(e) {
+        console.error(e)
+    }
+}
 
+export function sleep(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-export interface IUser extends Document {
+export interface IMovieSearch {
+    data: {
+        results: Array<ITmdb>
+    }
+}
+
+export interface MovieHelper {
+    results: Array<ITmdb>
+}
+
+export interface ITmdb {
+    id: number
+    original_language: string
+    original_title: string
+    overview: string
+    popularity: number
+    poster_path: string
+    release_date: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+}
+
+export interface IUser {
     _id: string;
     email: string;
     username: string;
--- src/pages/CreateMovie.tsx
+++ src/pages/CreateMovie.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {FormEvent, useState, useEffect} from 'react';
 import {useHistory} from 'react-router-dom'
-import { createMovie, refreshToken } from '../api';
+import { createMovie, ITmdb, refreshToken, searchMovies } from '../api';
 import '../index.css'
 
 export default function CreateMovie() {
@@ -12,6 +12,7 @@ export default function CreateMovie() {
     const [url, setUrl] = useState("")
     const [coverPicture, setCoverPicture] = useState("")
     const [loading, setLoading] = useState(false)
+    const [movies, setMovies] = useState<ITmdb[]>([])
 
     useEffect(() => {
         refreshToken().then((data) => {setUserId(data.user._id)}).catch(e => {history.push("/login")})
@@ -35,20 +36,40 @@ export default function CreateMovie() {
             alert(e)
         }
     }
+
+    const onTitleChange = async(e: any) => {
+        try {
+                setTitle(e.target.value)
+                let movies:Array<ITmdb>|undefined = await searchMovies(e.target.value)
+                setMovies(movies!)
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    const movieClick = (e:any, movie: ITmdb) => {
+        e.preventDefault()
+        console.log(movie)
+        setTitle(movie.title)
+        setDescription(movie.overview)
+        setCoverPicture(`https://www.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}`)
+        setMovies([])
+    }
+
     return (
         <div className="wrapper">
             <form className="login-form" onSubmit={(e) => submitHandle(e)}>
                 <label htmlFor="title">Title:</label>
-                <input id="title" type="text" placeholder="title" onChange={(e) => setTitle(e.target.value)}/>
+                <input id="title" autoComplete="off" type="text" value={title} placeholder="title" onChange={(e) => onTitleChange(e)}/>
                 <br/>
+                {movies? movies.map((movie) => (
+                    <button key={movie.id} onClick={(e) => movieClick(e, movie)}>{movie.title}</button>
+                )): null}
                 <label htmlFor="description">Description:</label>
-                <input id="description" type="text" placeholder="description" onChange={(e) => setDescription(e.target.value)}/>
+                <input id="description" type="text" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
                 <br/>
                 <label htmlFor="file">Movie File:</label>
                 {loading ? <p>Loading...</p>: <input type="file" id="file" placeholder="url" onChange={(e) => fileUpload(e)}/>}
-                <br/>
-                <label htmlFor="coverPicture">Cover Picture:</label>
-                <input type="text" id="coverPicture" placeholder="cover picture" onChange={(e) => setCoverPicture(e.target.value)}/>
                 <br/>
                 <input type="submit"/>
             </form>
