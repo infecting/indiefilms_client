e2c153531499cb3151630362dbe34189c9f6bef9
Lagoons Browning <lagoons@protonmail.com>
2021-01-28 22:12:56 -0600

    Load video better... not loading on desktop chrome

--- src/pages/MoviePlayer.tsx
+++ src/pages/MoviePlayer.tsx
@@ -14,15 +14,18 @@ export default function MoviePlayer({ match }:RouteComponentProps<TParams>) {
         coverPicture: "",
         score: 0
     })
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
+        setLoading(true )
         getMovie(match.params.id).then((data) => setMovie(data.movie)).catch((e) => console.error(e))
+        setLoading(false)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <div className="wrapper">
-            <video controls preload="metadeta" width="100%" height="auto">
-                <source key={movie._id} src={`${movie.url}#t=0.5`} type="video/mp4"/>
-            </video>
+            {loading ? <p>loading</p>:<video controls preload="metadeta" width="100%" height="auto">
+                <source key={movie._id} src={`${movie.url}#t=0.5`} onLoad={() => setLoading(false)} type="video/mp4"/>
+            </video>}
             <h1>{movie.title}</h1>
             <p>{movie.description}</p>
             <p>Posted by: {movie._id}</p>
