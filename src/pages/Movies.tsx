bd33d49799be690bce0cb2e55e9422c42c3604d3
Lagoons Browning <lagoons@protonmail.com>
2021-01-31 13:47:09 -0600

    Small changes to navbar

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -5,12 +5,10 @@ import '../index.css'
 
 export default function Movies() {
     const [movies, setMovies] = useState<IMovie[]>([]);
-    const [bannerPicture, setBannerPicture] = useState("");
 
     const movie = async() => {
         try {
             let res = await getMovies()
-            console.log(res.movies[0])
             setMovies(res.movies)
         } catch(e) {
             console.error(e)
@@ -25,13 +23,10 @@ export default function Movies() {
     
     useEffect(() => {
         movie()
-        setBannerPicture("https://media1.fdncms.com/pittsburgh/imager/u/original/17488698/movie_theater_reopening.jpg")
-    }, [movies])
+    }, [])
     return (
         <div>
-            <img src={bannerPicture} alt="banner" width="100%" height="300vh"/>
             <div className="wrapper">
-                
                 <div className="movies-wrapper">
                     {movies.map((movie) => (
                         <div key={movie._id}>
