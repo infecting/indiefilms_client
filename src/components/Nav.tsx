99daaa85c474baff9f0b6a5504258eeab5cabbb2
Lagoons Browning <lagoons@protonmail.com>
2021-02-14 15:11:24 -0600

    better looking home page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,7 +13,7 @@ export default function Nav() {
                 <nav>
                 <a href="/movies"><img className="logo" alt="Indiefilms" src={Logo} height="50%" width="50%"/></a>
                     <ul>
-                        <li className="items"><a href="/movies">Home</a></li>
+                        <li className="items"><a href="/movies">Movies</a></li>
                         <li className="items"><a href="/movies/new">Post Movie</a></li>
                         <li className="btn-item"><button className="btn-primary" onClick={() => redirect()}>Sign in</button></li>
                     </ul>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import {Link} from 'react-router-dom'
+import Img from '../assets/home.jpg';
 import '../index.css'
 
 export default function Home() {
     return (
-        <div className="home-wrapper">
-            <div className="wrapper">
-                <h1>IndieFilms</h1>
-                <p>Revolutionizing content consumption for indie filmmakers</p>
-                <Link to="/movies">Movies</Link>
+            <>
+            <img className="home-img" src={Img} alt=""/>
+            <div className="home-wrapper"> 
+                <div className="wrapper center">
+                    <h1>IndieFilms</h1>
+                    <p>Revolutionizing content consumption for indie filmmakers</p>
+                    <Link to="/movies" className="btn-primary black-text">Go to Movies</Link>
+                </div>
             </div>
-        </div>
+            </>
+        
     )
 }
