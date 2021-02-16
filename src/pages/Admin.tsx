73e7c32a05c014b74e5d7e8615d45f3cdd5465ec
Lagoons Browning <lagoons@protonmail.com>
2021-02-16 17:43:52 -0600

    add admin panel delete

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,5 @@
 import {FormEvent, useEffect, useState} from 'react';
-import { confirmMovies, getAdminMovies, IMovie, refreshToken } from '../api';
+import { confirmMovies, deleteMovie, getAdminMovies, IMovie, refreshToken } from '../api';
 import {useHistory} from 'react-router-dom'
 import Movie from '../components/Movie';
 
@@ -10,6 +10,14 @@ export default function Admin() {
     const confirm = async(e:FormEvent, id:string) => {
         let {accessToken} = await refreshToken();
         await confirmMovies(id, accessToken)
+        e.preventDefault()
+    }
+
+    const remove = async(e:FormEvent, id:string) => {
+        let {accessToken} = await refreshToken();
+        const newList = movies.filter((movie) => movie._id !== id);
+        setMovies(newList);
+        await deleteMovie(id, accessToken)
         e.preventDefault()
     }
 
@@ -33,7 +41,10 @@ export default function Admin() {
             {movies.map((movie) => (
                         <div className="movie" key={movie._id}>
                             <Movie key={movie._id} movie={movie}/>
+                            <br/>
                             <button onClick={e => confirm(e, movie._id)}>Confirm</button>
+                            <br/>
+                            <button onClick={e => remove(e, movie._id)} >Delete</button>
                         </div>
                     ))}
         </div>
